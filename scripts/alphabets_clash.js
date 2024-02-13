b5c37deb632d79370e7b57a42bb10de018141a05
Ahnaf Khan <ahnaf10100@gmail.com>
2024-02-14 02:13:43 +0600

    half of gameplay done

--- scripts/alphabets_clash.js
+++ scripts/alphabets_clash.js
@@ -8,8 +8,29 @@
 
 //Note: Due to the next process being a reuse of the whole of intersection we will use the below code for now
 
+function continueGame(){
+    const alphabet = getARandomAlphabet();
+    const display = document.getElementById("alphabet-random");
+    display.innerText = alphabet;
+    changeBackGroundColorById(alphabet);
+}
+
 function play(){
     hideElementById("home");
     addElementById("game");
+    continueGame();
 }
 
+function getARandomAlphabet(){
+    const alphabetString = "abcdefghijklmnopqrstuvwxyz";
+    const alphabets = alphabetString.split("");
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+    const alphabet = alphabets[index];
+    return alphabet;
+}
+
+function changeBackGroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add("bg-[#FFA500]")
+}
